d44d1ffae2d7a94cc731605775536dd9136cbe9f
Nathan <contact@nathanamick.com>
2024-09-28 11:56:07 -0400

    Add prettier, tailwind config and eslint

---
+++ tailwind.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from 'tailwindcss'
+import typography from '@tailwindcss/typography'
+import { createPreset } from 'fumadocs-ui/tailwind-plugin'
+
+const config: Config = {
+  content: [
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './content/**/*.{md,mdx}',
+    './mdx-components.{ts,tsx}',
+    './node_modules/fumadocs-ui/dist/**/*.js',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
+  },
+  plugins: [typography],
+  presets: [createPreset()],
+}
+
+export default config
